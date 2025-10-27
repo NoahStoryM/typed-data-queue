5cb309110e596c3ce06867fccf36c6c73b97c9d2
NoahStoryM <NoahStoryM@gmail.com>
2025-10-27 19:31:10 +0800

    Update

--- data/queue/private/types.rkt
+++ data/queue/private/types.rkt
@@ -1,9 +1,9 @@
 #lang typed/racket/base
 
 ;; TODO (Queueof t) → (Queueof t t)
-(struct (w r) Queueof ([_ : (Parameter w r)]))
+(struct (-t +t) Queueof ([_ : (Parameter -t +t)]))
 #;(struct (a ...) _ ([_ : (Parameter a ...)]) #:type-name Queueof) ; not work well
-(define-type (Queue w r) (Queueof w r)) ; avoid printing #(struct:Queueof ...)
+(define-type (Queue -t +t) (Queueof -t +t)) ; avoid printing #(struct:Queueof ...)
 (provide Queue (rename-out [Queue Queueof]))
 
 (define-type QueueTop (Queue Nothing Any))
